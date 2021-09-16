c0509a81a75ebc3a73ed355665b85b91af1d05fe
gorakhjoshi <>
2021-09-16 03:31:17 +0000

    array and array methods

---
+++ array.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const friend1 = 'Anjila';
+const friend2 = 'Riya';
+const friend3 = 'Sital';
+
+const friends = ['Anjila', 'Riya', 'Sital'];
+
+friends.push(5);
+console.log(typeof friends[friends.length - 2]);
+
+console.log(friends)
+
+
