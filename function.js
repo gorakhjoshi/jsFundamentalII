c7f021e774b716e3863ee3a48712a8cd63448489
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2021-09-16 03:10:59 +0000

    function completed

---
+++ function.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// const sum = 
+// console.log(myClass(4, 5))
+
+// function myClass(a, b) {
+//   return 'Hello'
+// }
+
+
+// myClass('Function expression')
+
+// const myClass = function (functionType) { console.log(functionType) }
+
+
+const myClass = (a, b) => {
+  return a + b
+}
+
+const sum = myClass(4, 5)
+
+console.log(sum)
+
+
+
