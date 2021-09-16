80b9948fd0c9e530653f334e8da06c1beac0c5ad
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2021-09-16 02:36:43 +0000

    Initial commit

---
+++ strict.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let lockdownSituations = false;
+const canDrive = true;
+
+if (canDrive) lockdownSituations = true;
+
+if (lockdownSituations) console.log("I can't drive");
+
+// const private = "Number"
